0951c397e40563cd875d1d36c7b907f86601ecbb
santhoshj001 <santhoshj296@gmail.com>
2022-06-06 20:57:23 +0530

    Added UnSplashApi

--- app/src/main/java/com/teamb/paging3sample/data/remote/UnSplashApi.kt
+++ app/src/main/java/com/teamb/paging3sample/data/remote/UnSplashApi.kt
@@ -1,19 +1,27 @@
 package com.teamb.paging3sample.data.remote
 
+import com.teamb.paging3sample.BuildConfig
 import com.teamb.paging3sample.model.UnsplashImage
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 
 interface UnSplashApi {
 
-   
+    @Headers("Authorization : Client-ID ${BuildConfig.API_KEY} ")
     @GET("/photos")
     suspend fun getAllImages(
         @Query("page") page: Int,
         @Query("per_page") pageSize: Int
     ): List<UnsplashImage>
 
+    @Headers("Authorization : Client-ID ${BuildConfig.API_KEY} ")
+    @GET("/search/photos")
+    suspend fun searchImages(
+        @Query("page") page: Int,
+        @Query("per_page") pageSize: Int
+    ): List<UnsplashImage>
 
 }
